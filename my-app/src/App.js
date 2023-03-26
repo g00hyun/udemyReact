5d375d2673050c070160ca52c5ed7bc68421d861
g00hyun <heroman1210@gmail.com>
2023-03-26 16:07:36 +0900

    [Update] setup file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,10 @@
-import './App.css';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState([]);
+  return(
+    <div>new React Project Page</div>
 
-  useEffect(() => {
-    fetch('http://222.107.240.29/hello-api?name=1')
-      .then(response => response.json())
-      .then(data => setData(data));
-  }, []);
-
-  return (
-    <div>
-      <h1>JSON Data</h1>
-      <ul>
-        {data.map(item => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  )
 }
 
 export default App;
